61d99422549d40bcb24524d1331b42a692312f08
CESCO <cescovalledev@gmail.com>
2023-06-02 22:31:06 -0600

    beggining new hook

---
+++ src/01-useState/CounterWithCustomHook.jsx
@@ -0,0 +1,11 @@
+export const CounterWithCustomHook = () => {
+  return (
+    <>
+      <h1>Counter With Custom Hook: XXX</h1>
+      <hr />
+      <button className="btn btn-primary">+1</button>
+      <button className="btn btn-light">Reset</button>
+      <button className="btn btn-secondary">-1</button>
+    </>
+  );
+};
