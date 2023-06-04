c99edde6b8e7849358e48b0373d15b40a2839419
CESCO <cescovalledev@gmail.com>
2023-06-03 22:14:23 -0600

    Adding a new functional component with password input

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,8 @@ import ReactDOM from "react-dom/client";
 // import { HooksApp } from "./HooksApp";
 // import { CounterApp } from "./01-useState/CounterApp";
 // import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
-import { SimpleForm } from "./02-useEffect/SimpleForm";
+// import { SimpleForm } from "./02-useEffect/SimpleForm";
+import { FormWithCustomHooks } from "./02-useEffect/FormWithCustomHooks";
 
 import "./index.css";
 
@@ -13,6 +14,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     {/* <HooksApp /> */}
     {/* <CounterApp /> */}
     {/* <CounterWithCustomHook /> */}
-    <SimpleForm />
+    {/* <SimpleForm /> */}
+    <FormWithCustomHooks />
   </React.StrictMode>
 );
