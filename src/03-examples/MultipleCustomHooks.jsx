a8cad43bd3106094da79498a1bbcf9be758d1822
CESCO <cescovalledev@gmail.com>
2023-06-04 22:43:44 -0600

    useFetch and useCounter together

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -1,15 +1,36 @@
+import { useCounter } from "../hooks/useCounter";
 import { useFetch } from "../hooks/useFetch";
 
 export const MultipleCustomHooks = () => {
-  const { data, hasError, isLoading } = useFetch(
-    "https://api.breakingbadquotes.xyz/v1/quotes"
-  );
+  const { counter, increment, decrement } = useCounter(1);
 
-  console.log({ data, hasError, isLoading });
+  const { data, hasError, isLoading } = useFetch(
+    `https://api.breakingbadquotes.xyz/v1/quotes/${counter}`
+  );
+  //   console.log({ data, hasError, isLoading });
+
+  //   const { author, quote } = !!data && data[0];
+
   return (
     <>
       <h1>Breaking Bad Quotes</h1>
       <hr />
+      <button onClick={() => increment()} className="btn btn-primary">
+        More Quotes
+      </button>
+      <button onClick={() => decrement()} className="btn btn-secondary">
+        Less Quotes
+      </button>
+      {isLoading ? (
+        <div className="alert alert-info text-center">Loading...</div>
+      ) : (
+        data.map((quote, i) => (
+          <blockquote key={i} className="blockquote text-end">
+            <p className="mb-4">{quote.quote}</p>
+            <footer className="blockquote-footer">{quote.author}</footer>
+          </blockquote>
+        ))
+      )}
     </>
   );
 };
