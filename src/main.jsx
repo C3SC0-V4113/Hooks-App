1d9e39f92853c95be95f0882d4827399abc07608
CESCO <cescovalledev@gmail.com>
2023-06-06 22:16:20 -0600

    intro to useReducer

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,9 @@ import ReactDOM from "react-dom/client";
 // import { Memorize } from "./06-memos/Memorize";
 // import { MemoHook } from "./06-memos/MemoHook";
 // import { CallbackHooks } from "./06-memos/CallbackHooks";
-import { Padre } from "./07-tarea-memo/Padre";
+// import { Padre } from "./07-tarea-memo/Padre";
+
+import "./08-useReducer/intro-reducer";
 
 import "./index.css";
 
@@ -29,6 +31,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     {/* <Memorize /> */}
     {/* <MemoHook /> */}
     {/* <CallbackHooks /> */}
-    <Padre />
+    {/* <Padre /> */}
   </React.StrictMode>
 );
