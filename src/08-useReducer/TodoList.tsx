4440bf42ffd6e1e0cf68ce251159e5d0d6f0d7b8
CESCO <cescovalledev@gmail.com>
2023-06-08 20:45:55 -0600

    handle Delete for Todos

--- src/08-useReducer/TodoList.tsx
+++ src/08-useReducer/TodoList.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { TodoItem } from "./TodoItem";
 
-export const TodoList = ({ todos }) => {
+export const TodoList = ({ todos, onDeleteTodo }) => {
   return (
     <ul className="list-group">
       {todos.map((todo) => (
-        <TodoItem todo={todo} key={todo.id} />
+        <TodoItem todo={todo} key={todo.id} onDeleteTodo={onDeleteTodo} />
       ))}
     </ul>
   );
