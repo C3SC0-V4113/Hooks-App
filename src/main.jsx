6cc62cf6d6ad6a8bd5ac56f40dcb5380c854e566
CESCO <cescovalledev@gmail.com>
2023-06-09 21:44:35 -0600

    added error page

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
 
 // import { HooksApp } from "./HooksApp";
 // import { CounterApp } from "./01-useState/CounterApp";
@@ -16,14 +20,25 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 // import { Padre } from "./07-tarea-memo/Padre";
 // import "./08-useReducer/intro-reducer";
 // import { TodoApp } from "./08-useReducer/TodoApp";
-import { MainApp } from "./09-useContext";
+import { AboutPage, HomePage, LoginPage, MainApp } from "./09-useContext";
 
 import "./index.css";
+import { ErrorPage } from "./09-useContext/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainApp />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      { path: "/about", element: <AboutPage /> },
+      { path: "/login", element: <LoginPage /> },
+      { path: "*", element: <Navigate to={"/"} replace /> },
+    ],
   },
 ]);
 
