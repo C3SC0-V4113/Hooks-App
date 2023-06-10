d646894e1dedbd2ae530ec2f6a497806bdb7893e
CESCO <cescovalledev@gmail.com>
2023-06-09 22:04:53 -0600

    clean coded for Navbar

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import { Navbar } from "./";
 
 export const MainApp = () => {
   return (
     <>
       <h1>MainApp</h1>
+      <Navbar />
       <hr />
       <Outlet />
     </>
