e4ffa238d4b4350e4a21dc31bf049c35a2a684aa
CESCO <cescovalledev@gmail.com>
2023-06-15 21:58:27 -0600

    Homework done

--- src/tests/09-useContext/HomePage.test.jsx
+++ src/tests/09-useContext/HomePage.test.jsx
@@ -3,6 +3,12 @@ import { HomePage } from "../../09-useContext/HomePage";
 import { UserContext } from "../../09-useContext/context/UserContext";
 
 describe("pruebas en <HomePage/>", () => {
+  const user = {
+    id: 1,
+    name: "Francisco",
+    email: "fran@gmail.com",
+  };
+
   test("debe de mostrar el componente sin el usuario", () => {
     render(
       <UserContext.Provider value={{ user: null }}>
@@ -11,6 +17,18 @@ describe("pruebas en <HomePage/>", () => {
     );
 
     const pretag = screen.getByLabelText("pre");
-    expect(pretag.innerHTML).toBe("null"); // screen.debug();
+    expect(pretag.innerHTML).toBe("null");
+    // screen.debug();
+  });
+
+  test("debe de mostrar el componente con el usuario", () => {
+    render(
+      <UserContext.Provider value={{ user }}>
+        <HomePage />
+      </UserContext.Provider>
+    );
+    screen.debug();
+    const pretag = screen.getByLabelText("pre");
+    expect(pretag.innerHTML).toBe(JSON.stringify(user, null, 3));
   });
 });
